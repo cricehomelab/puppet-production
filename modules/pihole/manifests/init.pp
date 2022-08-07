a34867cf394a55f40afa0cf38ac88cbb20466d43
Charlie Rice <charles.e.riceiii@gmail.com>
2022-08-07 09:59:01 -0400

    fixed

--- modules/pihole/manifests/init.pp
+++ modules/pihole/manifests/init.pp
@@ -1,7 +1,12 @@
 class pihole {  
   # need to create a conf file for unattended install https://discourse.pi-hole.net/t/what-is-setupvars-conf-and-how-do-i-use-it/3533
   # execute the installer script for pihole
+  
+  file { "Pi-hole":
+    ensure => directory,
+    source => 'puppet:///modules/pihole/Pi-hole'
+  }
   exec { 'install-pihole':
-    command => 'curl -sSL https://install.pi-hole.net | bash',    
+      
   }
 }
