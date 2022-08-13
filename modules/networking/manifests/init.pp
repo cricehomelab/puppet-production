e5e63ebf94e0096203c436a0fddb0f3154d20601
Charlie Rice <charles.e.riceiii@gmail.com>
2022-08-13 00:08:15 -0400

    i forgot to make it a class...

--- modules/networking/manifests/init.pp
+++ modules/networking/manifests/init.pp
@@ -1,49 +1,51 @@
 
-# filepaths to the files we are manipulating
-$hosts_file = '/etc/hosts'
-$ip_config_file = '/etc/netplan/50-cloud-init.yaml'
+class networking {
+  # filepaths to the files we are manipulating
+  $hosts_file = '/etc/hosts'
+  $ip_config_file = '/etc/netplan/50-cloud-init.yaml'
 
-# Setting dns to be equal to the pihole. 
-$ip_params = {
-  $ipaddress  => ::facts[networking][ip],
-  $gateway    => "192.168.50.1",
-  $macaddress => ::facts[networking][mac],
-  $dnsserver  => "192.168.50.128"
+  # Setting dns to be equal to the pihole. 
+  $ip_params = {
+    $ipaddress  => ::facts[networking][ip],
+    $gateway    => "192.168.50.1",
+    $macaddress => ::facts[networking][mac],
+    $dnsserver  => "192.168.50.128"
+  }
+
+  # configuration of hosts file. 
+  $hosts_params = {
+    $hostname  => ::facts[networking][hostname],
+    $fqdn      => ::facts[networking][fqdn],
+    $ipaddress => ::facts[networking][ip]
+  }
+
+  $ip_output    = epp('networking/50-cloud-init.yaml.epp', $ip_params)
+  $hosts_output = epp('networking/hosts.debian.tmpl.epp', $hosts_params)
+
+  # The goal here is to ensure the DNS is routing to our pihole. 
+  # Routing to pihole means we don't need DNS records on all machines. 
+  # This centralizes the DNS records.
+
+  # Part1: setting DNS correctly for device.
+  file { $ip_config_file :
+    ensure  => present,
+    content => $ip_output,  
+  }
+
+  # Part2: applying IP correctly. 
+  exec { 'netplan_application':
+    command     => 'netplan apply',
+    provider    => shell,
+    subscribe   => file[$ip_output],
+    refreshonly => true,
+  }
+
+  # Part3: Updating hosts
+  file { $hosts_file :
+    ensure  => present,
+    content => $hosts_output,
+    require => $ip_config_file,
+    audit  
+  }
+
 }
-
-# configuration of hosts file. 
-$hosts_params = {
-  $hostname  => ::facts[networking][hostname],
-  $fqdn      => ::facts[networking][fqdn],
-  $ipaddress => ::facts[networking][ip]
-}
-
-$ip_output    = epp('networking/50-cloud-init.yaml.epp', $ip_params)
-$hosts_output = epp('networking/hosts.debian.tmpl.epp', $hosts_params)
-
-# The goal here is to ensure the DNS is routing to our pihole. 
-# Routing to pihole means we don't need DNS records on all machines. 
-# This centralizes the DNS records.
-
-# Part1: setting DNS correctly for device.
-file { $ip_config_file :
-  ensure  => present,
-  content => $ip_output,  
-}
-
-# Part2: applying IP correctly. 
-exec { 'netplan_application':
-  command     => 'netplan apply',
-  provider    => shell,
-  subscribe   => file[$ip_output],
-  refreshonly => true,
-}
-
-# Part3: Updating hosts
-file { $hosts_file :
-  ensure  => present,
-  content => $hosts_output,
-  require => $ip_config_file,
-  audit  
-}
-
