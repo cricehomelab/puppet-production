cb94c957347f11f96dac15163cd725eb6e7ec2ca
Charlie Rice <charles.e.riceiii@gmail.com>
2022-08-14 11:07:52 -0400

    installing plex

--- modules/plex/files/plexinstall.sh
+++ modules/plex/files/plexinstall.sh
@@ -4,5 +4,4 @@
 cd /tmp/
 wget https://downloads.plex.tv/plex-media-server-new/1.19.3.2852-219a9974e/debian/plexmediaserver_1.19.3.2852-219a9974e_amd64.deb
 dpkg -i plexmediaserver_1.19.3.2852-219a9974e_amd64.deb
-systemctl enable plexmediaserver.service
-systemctl start plexmediaserver.service
+
