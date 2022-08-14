920687cd20231f58214f06384010f295b484614c
Charlie Rice <charles.e.riceiii@gmail.com>
2022-08-14 00:54:21 -0400

    plex install

--- modules/plex/manifests/init.pp
+++ modules/plex/manifests/init.pp
@@ -1,3 +1,27 @@
+# installation and config for plex. 
 class plex {
+  # on device paths
+  $plexlocation              = '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/'
+  $plexinstallscriptlocation = '/tmp/plexinstall.sh'
+
+  #files to service to machine
+  $plexinstallscript = 'puppet:///modules/plex/plexinstall.sh'
+
+  file { $plexinstallscriptlocation :
+    ensure => present,
+    source => $plexinstallscript,
+  }
+
+  exec { 'installplex' :
+    command  => 'sh /tmp/plexinstall.sh',
+    provider => shell,
+    creates  => $plexlocation,
+    require  => $plexinstallscriptlocation,    
+  }
+
+  service {'plexmediaserver':
+    ensure => running,
+  }
+
 
 }
