df47cdb5df004cd3c9e283449e0bdbe990badb2f
Charlie Rice <charles.e.riceiii@gmail.com>
2022-08-16 18:17:32 -0400

    jellyfin install process.

---
+++ modules/jellyfin/files/jellyfininstallrequirements.sh
@@ -0,0 +1,5 @@
+#! bash.sh
+add-apt-repository universe
+curl -fsSL https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/debian-jellyfin.gpg
+echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/ubuntu $( lsb_release -c -s ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+apt update
--- modules/jellyfin/manifests/init.pp
+++ modules/jellyfin/manifests/init.pp
@@ -1,6 +1,36 @@
 class jellyfin {
-  service { 'jellyfin':
+  $scriptinstallsource = 'puppet:///jellyfin/modules/jellyfininstallrequirements.sh'
+  $prerequisiteinstall = '/tmp/jellyfininstallrequirements.sh'
+  
+   
+  # first requirement
+  package { 'apt-transport-https':
+    ensure => installed,
+  }
+  # staging install script
+  file { $prerequisiteinstall :
+    ensure => present,
+    source => $scriptinstallsource,
+    require => Package['apt-transport-https'],
+  }
+  # running the install script
+  exec { 'install-prerequisite-command':
+    command  => 'sh /tmp/jellyfininstallrequirements.sh',
+    provider => shell,
+    creates  => '/etc/apt/sources.list.d/jellyfin.list',
+    require  => File[$prerequisiteinstall],
+  }
+  # running the jellyfin install
+  package { 'jellyfin-install':
+    name    => 'jellyfin',
+    ensure  => installed,    
+    require => Exec['install-prerequisite-command'],
+  }
+  # ensuring the service stays started once installed. 
+  service { 'jellyfin': 
     ensure => running,
-    enable => true,
-  } 
+    require => Package['jellyfin-install'],
+  }
+  
 }
+  
