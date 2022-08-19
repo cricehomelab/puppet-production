d91a7db246dd61876c5a77a4d8516ed606fc8cc6
Charlie Rice <charles.e.riceiii@gmail.com>
2022-08-18 21:00:40 -0400

    fixing disk mount

--- modules/disks/manifests/init.pp
+++ modules/disks/manifests/init.pp
@@ -1,8 +1,9 @@
 class disks { 
 
   exec { 'mount-disk'
-      command => 'mount /dev/sdb1 /media/movies',
-      unless => 'test -d /home/movies/Anime',
+      command  => 'mount /dev/sdb1 /media/movies',
+      provider => shell,
+      unless   => 'test -d /home/movies/Anime',
   }
 
 }
