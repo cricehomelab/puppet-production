a5531113b0776f92c9db664980410a7330479f0f
Charlie Rice <charles.e.riceiii@gmail.com>
2022-11-05 14:58:15 -0400

    remove excess whitespace.

--- modules/updates/manifests/init.pp
+++ modules/updates/manifests/init.pp
@@ -1,34 +1,28 @@
 class updates {
   $restart_group_1 = ['janus-main', 'dionysus-main']
   $restart_group_2 = ['iana-main', 'preprodtest'] 
-  if $::facts['hostname'] in $dns_group {
-    schedule { 'pihole_update' :
-      period => weekly,
-      range  => "23:00 - 1:00",
-      repeat => 1,
-    }
-  }
   if $::facts['hostname'] in $restart_group_1 {
-    schedule { 'group1updates' :
+    schedule { 'group1updates':
       period => weekly,
       range  => "3:00 - 5:00",
       repeat => 1,
     }
-  }
+  }  
   elsif $::facts['hostname'] in $restart_group_2 {
-    schedule { "group2updates" :
+    schedule { "group2updates":
       period => weekly, 
       range  => "1:00 - 3:00",
       repeat => 1,
     }
-  }   
+  }
+
   # weekly command to run updates. 
-  exec {'apt-get update && apt-get upgrade' :
+  exec {'apt-get update && apt-get upgrade':
     schedule => ['group1updates', 'group2updates'],
     provider => shell,
   }
   # reboot after updates go through.
-  reboot {'after' :
+  reboot {'after':
     subscribe => Exec['apt-get update && apt-get upgrade'],
   }
 }
