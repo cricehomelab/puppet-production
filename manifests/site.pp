282ecbf3d5f2597f02a4d8b56ee3884abc298c21
Charlie Rice <charles.e.riceiii@gmail.com>
2022-11-06 11:15:42 -0500

    updating reposynch

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,11 +2,11 @@
 # this node is for pihole. 
 node 'janus-main.local', 'iana-main.local' {
     include pihole
-    include networking    
+    include networking
 #    include updates
 }
 
-node "dionysus-main.local" {    
+node "dionysus-main.local" {
     include jellyfin 
     include networking
     include disks
@@ -14,12 +14,13 @@ node "dionysus-main.local" {
 }
 
 node "preprodtest"{
-    include networking    
+    include networking
 }
 
 node "geppetto-main.local" {
 #    include updates
     include reposync
+
 }
 
 # setting a default node.
