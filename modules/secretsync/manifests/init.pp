9c3c4611511904724e8a9990fb7fbf0809a06557
Charlie Rice <charles.e.riceiii@gmail.com>
2022-11-28 19:53:29 -0500

    working on secrets modules

--- modules/secretsync/manifests/init.pp
+++ modules/secretsync/manifests/init.pp
@@ -8,11 +8,12 @@ class secretsync {
     source => 'puppet:///modules/secretsync/secretsync.sh',
   }
 
-  file { '/etc/secrets/git_init.sh' :
+  file { '/tmp/secrets/git_init.sh' :
     ensure => file,
     source => 'puppet:///modules/secretsync/git_init.sh',
   }
-  exec { 'sh /etc/secretes/git_init.sh' :
+
+  exec { 'sh /tmp/secrets/git_init.sh' :
     provider => shell,
     unless   => 'test -f /etc/pihole/setupvars.conf',
   }
