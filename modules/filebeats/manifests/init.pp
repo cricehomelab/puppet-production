5835a25bb8965d0274d255f6ad64d5ab0e843d23
Charlie Rice <charles.e.riceiii@gmail.com>
2022-11-30 17:43:04 -0500

    filebeats module.

--- modules/filebeats/manifests/init.pp
+++ modules/filebeats/manifests/init.pp
@@ -1,5 +1,17 @@
 class filebeats{
 
+  #install portion
+  file { '/etc/scripts/install_filebeats.sh':
+    ensure => present,
+    source => 'puppet:///modules/filebeats/installfilebeats.sh'
+  }
+
+  exec { '/etc/scripts/install_filebeats.sh':
+    provider => shell,
+    creates => '/etc/filebeats/filebeat.yml',
+  }
+
+  # state of service.
   service {'filebeat' :
     ensure => running,
   }
