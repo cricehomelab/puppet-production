30a4c4625066881939707f14c13d4a838f0bd2fd
Charlie Rice <charles.e.riceiii@gmail.com>
2022-12-03 14:42:21 -0500

    fixing secret sync module.

--- modules/filebeats/files/installfilebeats.sh
+++ modules/filebeats/files/installfilebeats.sh
@@ -3,7 +3,7 @@
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.5.2-amd64.deb
 sudo dpkg -i filebeat-8.5.2-amd64.deb
 
-cp /tmp/secrets/filebeat/filebeat.yml /etc/filebeat/filebeat.yml
+cp /tmp/secrets/puppet_secretrepo/filebeat/filebeat.yml /etc/filebeat/filebeat.yml
 
 systemctl start filebeat
 
