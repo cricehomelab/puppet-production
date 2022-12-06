b4a6de0c5d637e055bf9229506ca658867dd2128
Charlie Rice <charles.e.riceiii@gmail.com>
2022-12-05 19:08:53 -0500

    update to kibana and elasticsearch on seshat-main

--- modules/logging/manifests/init.pp
+++ modules/logging/manifests/init.pp
@@ -1,15 +1,19 @@
 class logging {
-  file { '/etc/scripts/elk_server_install.sh' :
-    ensure => present,
-    source => 'puppet:///modules/logging/elkserverinstall.sh',
-  }
-
-  exec { 'sh /etc/scripts/elk_server_install.sh' :
-    provider => shell,
-    unless => 'test -f /home/charlie/elastisearchinfo.txt'
-  }
+  #file { '/etc/scripts/elk_server_install.sh' :
+  #  ensure => present,
+  #  source => 'puppet:///modules/logging/elkserverinstall.sh',
+  #}
+#
+  #exec { 'sh /etc/scripts/elk_server_install.sh' :
+  #  provider => shell,
+  #  unless => 'test -f /home/charlie/elastisearchinfo.txt'
+  #}
 
   service { 'elasticsearch':
+    ensure => running,
+  }
+
+  service { 'kibana' :
     ensure => running,
   }
 }
